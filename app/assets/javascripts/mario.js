aac7d28574b0d3dac958b0fd66be9b1e7bc9f2ab
DazLamas <sarah.d.lamas@gmail.com>
2014-11-20 12:57:51 +0100

    separate pixel_jail and mario_platform game in different routes and views

--- app/assets/javascripts/mario.js
+++
@@ -1,7 +0,0 @@
-
-
-
-// New game inherit from phaser.js
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example');
-game.state.add('level1', States.LevelExample);
-game.state.start('level1');
